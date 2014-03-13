370a4b90961ad07dee35f68206c4d8929c1a09bd
kestefanie <yucong880@gmail.com>
2014-03-13 18:17:32 -0400

    Add index type and arguments to IndexedColumn

--- src/ca/mcgill/distsys/hbase96/indexcommonsinmem/IndexedColumn.java
+++ src/ca/mcgill/distsys/hbase96/indexcommonsinmem/IndexedColumn.java
@@ -3,21 +3,43 @@ package ca.mcgill.distsys.hbase96.indexcommonsinmem;
 import java.io.Serializable;
 
 public class IndexedColumn implements Serializable {
-    private static final long serialVersionUID = 110709434738361949L;
+	private static final long serialVersionUID = 110709434738361949L;
 
-    private byte[] columnFamily;
-    private byte[] qualifier;
-    
-    public IndexedColumn(byte[] columnFamily, byte[] qualifier) {
-        this.columnFamily = columnFamily;
-        this.qualifier = qualifier;
-    }
+	private byte[] columnFamily;
+	private byte[] qualifier;
 
-    public byte[] getColumnFamily() {
-        return columnFamily;
-    }
+	private String indexType;
+	private Object[] arguments;
 
-    public byte[] getQualifier() {
-        return qualifier;
-    }
+	public IndexedColumn(byte[] columnFamily, byte[] qualifier) {
+		this.columnFamily = columnFamily;
+		this.qualifier = qualifier;
+		// set default index type and arguments
+		indexType = "ca.mcgill.distsys.hbase96.indexcoprocessorsinmem.pluggableIndex.hybridBased.HybridIndex";
+		arguments = new Object[] {columnFamily, qualifier};
+	}
+
+	public byte[] getColumnFamily() {
+		return columnFamily;
+	}
+
+	public byte[] getQualifier() {
+		return qualifier;
+	}
+	
+	public String getIndexType() {
+		return indexType;
+	}
+	
+	public Object[] getArguments() {
+		return arguments;
+	}
+	
+	public void setIndexType(String type){
+		indexType = type;
+	}
+	
+	public void setArguments(Object[] argu) {
+		arguments = argu;
+	}
 }
